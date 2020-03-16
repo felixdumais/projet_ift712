e315d207ce1da19cdb5f04b57dd975a95186db18
Felix Dumais <felix.dumais@usherbrooke.ca>
2020-03-16 12:47:43 -0400

    change le svm pour rbf

--- src/models/SVMClassifier.py
+++ src/models/SVMClassifier.py
@@ -1,6 +1,6 @@
 from src.models.Classifier import Classifier
 from sklearn.multiclass import OneVsRestClassifier
-from sklearn.svm import LinearSVC
+from sklearn.svm import SVC
 from sklearn.model_selection import cross_val_score
 
 from sklearn.model_selection import train_test_split
@@ -11,12 +11,12 @@ import numpy as np
 class SVMClassifier(Classifier):
     def __init__(self, X_train, X_test, y_train, y_test, loss):
         super().__init__(X_train, X_test, y_train, y_test, loss)
-        self.classifier = OneVsRestClassifier(LinearSVC(loss=self.loss, verbose=True, tol=0.001))
+        self.classifier = OneVsRestClassifier(SVC(kernel='rbf', verbose=True, tol=0.001))
         self.kfolded = False
 
     def train(self):
-        if self.kfolded is False:
-            self._research_hyperparameter()
+        # if self.kfolded is False:
+        #     self._research_hyperparameter()
         self.classifier.fit(self.X_train, self.y_train)
 
     def predict(self):
@@ -32,20 +32,28 @@ class SVMClassifier(Classifier):
     def _research_hyperparameter(self):
         self.kfolded = True
         current_score = None
-        for i in range(1, 3):
-            C = 0.1 * i
-            self.classifier.set_params(estimator__C=C)
-            scores = cross_val_score(self.classifier, self.X_train, self.y_train, cv=2, verbose=1, n_jobs=4)
+        for i in range(1):
+            C = 0.001*10**i
+            for j in range(1):
+                gamma = 0.001*10**j
+                self.classifier.set_params(estimator__C=C, estimator__gamma=gamma)
+                scores = cross_val_score(self.classifier, self.X_train, self.y_train, cv=5, verbose=1, n_jobs=-1)
 
-            mean_score = scores.mean()
-            if current_score is None:
-                best_C = C
-                current_score = mean_score
-            elif mean_score > current_score:
-                best_C = C
-                current_score = mean_score
+                mean_score = scores.mean()
+                print('C = {}'.format(C))
+                print('gamma = {}'.format(gamma))
+                print('scores = {}'.format(scores))
+                if current_score is None:
+                    best_C = C
+                    best_gamma = gamma
+                    current_score = mean_score
+                elif mean_score > current_score:
+                    best_C = C
+                    best_gamma = gamma
+                    current_score = mean_score
 
-        self.classifier.set_params(estimator__C=best_C)
+        self.classifier.set_params(estimator__C=best_C, estimator__gamma=best_gamma)
+
 
 
 
