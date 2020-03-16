7ddd0011ea2ce55b987c0deccd3b1cf4b7e4dd08
Felix Dumais <felix.dumais@usherbrooke.ca>
2020-03-16 14:51:27 -0400

    mute _research_hyperparameter

--- src/models/SVMClassifier.py
+++ src/models/SVMClassifier.py
@@ -1,5 +1,6 @@
 from src.models.Classifier import Classifier
 from sklearn.multiclass import OneVsRestClassifier
+from sklearn.multioutput import MultiOutputClassifier
 from sklearn.svm import SVC
 from sklearn.model_selection import cross_val_score
 
@@ -11,7 +12,7 @@ import numpy as np
 class SVMClassifier(Classifier):
     def __init__(self, X_train, X_test, y_train, y_test, loss):
         super().__init__(X_train, X_test, y_train, y_test, loss)
-        self.classifier = OneVsRestClassifier(SVC(kernel='rbf', verbose=True, tol=0.001))
+        self.classifier = OneVsRestClassifier(SVC(kernel='linear', verbose=False, tol=0.001))
         self.kfolded = False
 
     def train(self):
@@ -32,17 +33,18 @@ class SVMClassifier(Classifier):
     def _research_hyperparameter(self):
         self.kfolded = True
         current_score = None
-        for i in range(1):
-            C = 0.001*10**i
-            for j in range(1):
-                gamma = 0.001*10**j
-                self.classifier.set_params(estimator__C=C, estimator__gamma=gamma)
-                scores = cross_val_score(self.classifier, self.X_train, self.y_train, cv=5, verbose=1, n_jobs=-1)
+        for i in range(2):
+            C = 0.0001*10**i
+            for j in range(2):
+                gamma = 0.0001*10**j
+                self.classifier.set_params(estimator__estimator__C=C, estimator__estimator__gamma=gamma)
+                scores = cross_val_score(self.classifier, self.X_train, self.y_train, cv=5, verbose=False, n_jobs=-1)
 
                 mean_score = scores.mean()
                 print('C = {}'.format(C))
                 print('gamma = {}'.format(gamma))
                 print('scores = {}'.format(scores))
+                print('mean_scores = {}'.format(mean_score))
                 if current_score is None:
                     best_C = C
                     best_gamma = gamma
@@ -52,7 +54,7 @@ class SVMClassifier(Classifier):
                     best_gamma = gamma
                     current_score = mean_score
 
-        self.classifier.set_params(estimator__C=best_C, estimator__gamma=best_gamma)
+        self.classifier.set_params(estimator__estimator__C=best_C, estimator__estimator__gamma=best_gamma)
 
 
 
