dc0953b67b60d7405df9ebcca27e380eb588cd8a
Felix Dumais <felix.dumais@usherbrooke.ca>
2020-03-17 18:08:07 -0400

    use GridSearchCV

--- src/models/SVMClassifier.py
+++ src/models/SVMClassifier.py
@@ -4,6 +4,7 @@ from sklearn.multioutput import MultiOutputClassifier
 from sklearn.svm import SVC
 from sklearn.model_selection import cross_val_score
 from skmultilearn.problem_transform import BinaryRelevance
+from sklearn.model_selection import GridSearchCV
 
 from sklearn.model_selection import train_test_split
 import pandas as pd
@@ -34,29 +35,35 @@ class SVMClassifier(Classifier):
 
     def _research_hyperparameter(self):
         self.kfolded = True
-        current_score = None
-        for i in range(6):
-            C = 0.000001*10**i
-            for j in range(6):
-                gamma = 0.000001*10**j
-                self.classifier.set_params(classifier__C=C, classifier__gamma=gamma)
-                scores = cross_val_score(self.classifier, self.X_train, self.y_train, cv=5, verbose=False, n_jobs=-1)
+        # current_score = None
+        # for i in range(6):
+        #     C = 0.000001*10**i
+        #     for j in range(6):
+        #         gamma = 0.000001*10**j
+        #         self.classifier.set_params(classifier__C=C, classifier__gamma=gamma)
+        #         scores = cross_val_score(self.classifier, self.X_train, self.y_train, cv=5, verbose=False, n_jobs=-1)
+        #
+        #         mean_score = scores.mean()
+        #         print('C = {}'.format(C))
+        #         print('gamma = {}'.format(gamma))
+        #         print('scores = {}'.format(scores))
+        #         print('mean_scores = {}'.format(mean_score))
+        #         if current_score is None:
+        #             best_C = C
+        #             best_gamma = gamma
+        #             current_score = mean_score
+        #         elif mean_score > current_score:
+        #             best_C = C
+        #             best_gamma = gamma
+        #             current_score = mean_score
+        #
+        # self.classifier.set_params(classifier__C=best_C, classifier__gamma=best_gamma)
 
-                mean_score = scores.mean()
-                print('C = {}'.format(C))
-                print('gamma = {}'.format(gamma))
-                print('scores = {}'.format(scores))
-                print('mean_scores = {}'.format(mean_score))
-                if current_score is None:
-                    best_C = C
-                    best_gamma = gamma
-                    current_score = mean_score
-                elif mean_score > current_score:
-                    best_C = C
-                    best_gamma = gamma
-                    current_score = mean_score
-
-        self.classifier.set_params(classifier__C=C, classifier__gamma=gamma)
+        C = [0.00001*10**x for x in list(range(7))]
+        parameters = {'classifier__C': C}
+        self.classifier = GridSearchCV(self.classifier, parameters, n_jobs=-1, verbose=1)
+        self.classifier.fit(self.X_train, self.y_train)
+        print(self.classifier.cv_results_)
 
 
 
