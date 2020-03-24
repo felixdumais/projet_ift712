97571cca7c3dbc4918dba7d04661da33553ad5ae
JoelleFV <joelle.frechette-viens@usherbrooke.ca>
2020-03-23 22:27:35 -0400

    optimisé

--- src/models/RBF.py
+++ src/models/RBF.py
@@ -1,7 +1,9 @@
 from src.models.Classifier import Classifier
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.gaussian_process.kernels import RBF
+from sklearn.gaussian_process import GaussianProcessClassifier
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
 
 from sklearn.model_selection import train_test_split
 import pandas as pd
@@ -10,13 +12,17 @@ import numpy as np
 
 class RBFClassifier(Classifier):
     def __init__(self, X_train, X_test, y_train, y_test, loss):
+
         super().__init__(X_train, X_test, y_train, y_test, loss)
-        self.classifier = OneVsRestClassifier(RBF(length_scale=1.0, length_scale_bounds=(1e-05, 100000.0)))
-        self.kfolded = False
+        kernel = RBF(length_scale=1.0)
+        self.classifier = OneVsRestClassifier(GaussianProcessClassifier(kernel=kernel, optimizer='fmin_l_bfgs_b', n_restarts_optimizer=10, max_iter_predict=10, warm_start=False, copy_X_train=True, random_state=None, n_jobs=2))
+        # multi_class='one_vs_rest', 
+        #OneVsRestClassifier(
+        self.kfolded = True
 
     def train(self):
-        #if self.kfolded is False:
-        #    self._research_hyperparameter()
+        if self.kfolded is False:
+            self._research_hyperparameter()
         self.classifier.fit(self.X_train, self.y_train)
 
     def predict(self):
@@ -30,22 +36,22 @@ class RBFClassifier(Classifier):
         return self.classifier
 
     def _research_hyperparameter(self):
-        self.kfolded = True
-        current_score = None
-        for i in range(1, 3):
-            C = 0.1 * i
-            self.classifier.set_params(estimator__C=C)
-            scores = cross_val_score(self.classifier, self.X_train, self.y_train, cv=2, verbose=1, n_jobs=4)
+        n_restarts_optimizer = [10**x for x in list(range(1,3))]
+        max_iter_predict = [10**x for x in list(range(1,3))]
+        parameters = [{'estimator__n_restarts_optimizer': n_restarts_optimizer,'estimator__max_iter_predict': max_iter_predict}]
 
-            mean_score = scores.mean()
-            if current_score is None:
-                best_C = C
-                current_score = mean_score
-            elif mean_score > current_score:
-                best_C = C
-                current_score = mean_score
-
-        self.classifier.set_params(estimator__C=best_C)
-
+        self.classifier = GridSearchCV(self.classifier, parameters,
+                                       n_jobs=1,
+                                       verbose=2,
+                                       cv=3,
+                                       return_train_score=True,
+                                       scoring='precision_macro')
+        self.classifier.fit(self.X_train, self.y_train)
+        print('Cross validation result')
+        print(self.classifier.cv_results_)
+        print('Best estimator: {}'.format(self.classifier.best_estimator_))
+        print('Best score: {}'.format(self.classifier.best_score_))
+        print('Best hyperparameters: {}'.format(self.classifier.best_params_))
+        print('Refit time: {}'.format(self.classifier.refit_time_))
 
 
